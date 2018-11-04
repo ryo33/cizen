4479258f792f58d324610e5cf51ff6532a790c4e
Hashiguchi <ryo12redstone@gmail.com>
2018-11-04 11:15:37 +0900

    Version 0.12.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cizen.MixProject do
   def project do
     [
       app: :cizen,
-      version: "0.11.4",
+      version: "0.12.0",
       package: package(),
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -177,6 +177,7 @@ defmodule Cizen.MixProject do
         Cizen.CizenSagaRegistry,
         Cizen.CrashLogger,
         Cizen.EndSaga,
+        Cizen.ForkSaga,
         Cizen.MonitorSaga,
         Cizen.MonitorSaga.Down,
         Cizen.Saga,
